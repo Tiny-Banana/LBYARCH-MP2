9ab3c58ea9a989150105cff3a8dbc390249822d4
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-04-02 06:25:12 +0800

    refactor asm file

--- LBYARCH-MP2/func.asm
+++ LBYARCH-MP2/func.asm
@@ -8,46 +8,46 @@ default rel
 global stencilComputation
 
 stencilComputation:
-	push rsi
+    push rsi
     push rbx
-	push r14
-	push r15
-	push rbp 
+    push r14
+    push r15
+    push rbp 
     mov rbp, rsp
     add rbp, 16
     add rbp, 32 
 
-	lea r14, [rcx]							;pointer to X
-	lea r15, [rdx]							;pointer to Y
-	mov r10, r8							    ;stencil loop
-	mov r11, 0								;counter of stencil loop
-	movsd xmm0, [stencilSumSet]				;stencil window sum
+    lea r14, [rcx]					;pointer to X
+    lea r15, [rdx]					;pointer to Y
+    mov r10, r8			                         ;stencil loop
+    mov r11, 0						;counter of stencil loop
+    movsd xmm0, [stencilSumSet]				;stencil window sum
 
 initialize:
-	mov rcx, 7
+    mov rcx, 7
 loopX:
-	movsd xmm1, [r14]
-	addsd xmm0, xmm1
-	add r14, 8
-	loop loopX
+    movsd xmm1, [r14]
+    addsd xmm0, xmm1
+    add r14, 8
+    loop loopX
 
-	movsd [r15], xmm0	;write to Y
-	add r15, 8 			;inc pointer to Y
+    movsd [r15], xmm0	;write to Y
+    add r15, 8 		;inc pointer to Y
 
-	inc r11
-	cmp r10, r11
-	jz tapos
+    inc r11
+    cmp r10, r11
+    jz tapos
 
-	mov rcx, 6
+    mov rcx, 6
 
 resetLoopX:
-	sub r14, 8
-	loop resetLoopX
-	movsd xmm0, [stencilSumSet] ;reset stencil window sum
-	jmp initialize
+    sub r14, 8
+    loop resetLoopX
+    movsd xmm0, [stencilSumSet] ;reset stencil window sum
+    jmp initialize
 	
 tapos:
-	 pop rbp
+     pop rbp
      pop r14
      pop r15
      pop rbx
